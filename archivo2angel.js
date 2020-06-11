c893fcf05c77a61a27cb3980552483379ad270ec
Angel Gimenez <angelgimenez992@gmail.com>
2020-06-11 12:15:47 -0300

    commit angel

---
+++ archivo2angel.js
@@ -0,0 +1,6 @@
+let auto = {
+    ruedas: 4,
+    puertas: 2,
+    marca: "BMW",
+    pagado: true
+}
---
+++ archivo3angel.js
@@ -0,0 +1,8 @@
+let calculadora = {
+    sumar: function(n1, n2){
+        return n1 + n2
+    },
+    resta: function(n1, n2){
+        return n1 - n2
+    }
+}
